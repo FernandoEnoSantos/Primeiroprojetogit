272d469436e3110fe3b612428718360181bf0ee2
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-03-14 10:32:28 -0300

    Criado o primeiro projeto com c#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace primeiroprog{
+
+    class Program{
+        static void Main(string[] args){
+
+                System.Console.WriteLine("Hello World Program!!");
+
+
+        }
+
+    }
+}
